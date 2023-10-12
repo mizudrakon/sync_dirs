79615bc7283474bf2fc4128c23d00c6195c7b626
mizudrakon <mizudrakon@gmail.com>
2023-10-12 16:16:29 +0200

    setting it up as a module while progressing with sync.py

--- sync_py/src/sync.py
+++ sync_py/src/sync.py
@@ -2,10 +2,11 @@
 import os
 import sys
 import shutil
+import datetime
 
 # t is the required number of \t when printing, f is just check if we're supposed to print files
 def printls(path = '.',tab='\t', t = 0,f = True):
-    """function to go through a file system tree given an address"""
+    """function to simply write out a folder contents recursively"""
     # make a list using os.scandir()
     L = [e for e in os.scandir(path)]
     for e in L:
@@ -22,27 +23,34 @@ def printls(path = '.',tab='\t', t = 0,f = True):
             print(e.name + '/')
             printls(path + '/' + e.name,tab,t+1,f)
 
-
-#making a function that will copy the entire tree from root
-def copy(origin = '.', replica = '../..', out = './log'):
-    #make the root dir
-    origin = os.path.abspath(origin)
-    origin_name = origin.split('/')[-1]
-    replica = os.path.abspath(replica)
-    os.mkdir(replica + '/' + origin_name, os.stat(origin).st_mode)
-    
+def copy_folder_content(origin, replica, out):
+    """Copy contents of the origin folder to the replica folder"""
     L = [e for e in os.scandir(origin)]
     for e in L:
         #skip symlinks or hidden files
         if e.is_symlink() or e.name[0] == '.':
             continue
-        #if file and we want file: print
+        #copy files and write message
         if e.is_file():
             shutil.copy2(e,replica) 
-            print(e.name + ' modified:' + str(e.stat().st_mtime))
-        #if dir go deeper
+            print(datetime.datetime.now() + ' ' + e.path + ' copied')
+        #make dir with same rights and enter copy it's contents too
         if e.is_dir():
-            print(e.name + '/')
+            print(datetime.datetime.now() + ' ' + e.path + '/')
+            os.mkdir(replica + '/' + e.name,e.stat().st_mode)
+            copy_folder_content(origin + '/' + e.name, replica + '/' + e.name,out)
+
+
+#first preparation function for the copy_folder_content
+def copy_folder(origin = '.', replica = '../..', out = './log'):
+    """Creates or enters the replica folder and starts the process"""
+    #make the root dir
+    origin = os.path.abspath(origin)
+    origin_name = origin.split('/')[-1]
+    replica = os.path.abspath(replica)
+    replica = replica + '/' + origin_name 
+    os.mkdir(replica, os.stat(origin).st_mode)
+    copy_folder_content(origin + '/' + origin_name, replica, out)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
