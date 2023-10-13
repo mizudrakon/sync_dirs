76c5287050500f109f88cb6642f37911b8489c20
mizudrakon <mizudrakon@gmail.com>
2023-10-13 13:38:17 +0200

    copying done, working on removal

--- sync_py/src/sync.py
+++ sync_py/src/sync.py
@@ -3,6 +3,7 @@ import os
 import sys
 import shutil
 import datetime
+import time
 
 # t is the required number of \t when printing, f is just check if we're supposed to print files
 def printls(path = '.',tab='\t', t = 0,f = True):
@@ -23,42 +24,68 @@ def printls(path = '.',tab='\t', t = 0,f = True):
             print(e.name + '/')
             printls(path + '/' + e.name,tab,t+1,f)
 
+def remove_dir(dir_path):
+    return True
+
 def copy_folder_content(origin, replica, out):
     """Copy contents of the origin folder to the replica folder"""
-    L = [e for e in os.scandir(origin)]
-    for e in L:
+    Ls = [entry for entry in os.scandir(origin)]
+    ###
+    replica_LS = [entry.name for entry in os.scandir(replica)]
+    At = [False]*len(replica_LS)
+    ###
+    for entry in Ls:
         #skip symlinks or hidden files
-        if e.is_symlink() or e.name[0] == '.':
+        if entry.is_symlink() or entry.name[0] == '.':
             continue
-        #copy files and write message
-        if e.is_file():
-            shutil.copy2(e,replica) 
-            print(datetime.datetime.now() + ' ' + e.path + ' copied')
+        
+        #cataloguing files and dirs for possible removal
+        if entry.name in replica_LS:
+            At[replica_LS.index(entry.name)] = True
+        
+        replica_entry_path = replica + '/' + entry.name
+        if entry.is_file():
+            if not os.path.isfile(replica_entry_path) or entry.stat().st_mtime > os.stat(replica_entry_path).st_mtime:
+                shutil.copy2(entry,replica) 
+                print(str(datetime.datetime.now()) + ' ' + entry.path + ' copied file')
         #make dir with same rights and enter copy it's contents too
-        if e.is_dir():
-            print(datetime.datetime.now() + ' ' + e.path + '/')
-            os.mkdir(replica + '/' + e.name,e.stat().st_mode)
-            copy_folder_content(origin + '/' + e.name, replica + '/' + e.name,out)
+        if entry.is_dir():
+            if not os.path.isdir(replica_entry_path):
+                os.mkdir(replica_entry_path,entry.stat().st_mode)
+                #shutil.copystat(origin + '/' + entry.name,replica_entry_path)
+                #print(str(os.stat(replica_entry_path).st_mtime) + ' ' + str(entry.stat().st_mtime))
+                print(str(datetime.datetime.now()) + ' ' + replica_entry_path + ' made folder')
+            copy_folder_content(origin + '/' + entry.name, replica_entry_path, out)
+    #print(At) 
+    for entry in replica_LS:
+        if not At[replica_LS.index(entry)]:
+            os.remove(replica + '/' + entry)
+            print(str(datetime.datetime.now()) + ' ' + replica + '/' + entry + ' removed')
+
 
 
 #first preparation function for the copy_folder_content
 def copy_folder(origin = '.', replica = '../..', out = './log'):
     """Creates or enters the replica folder and starts the process"""
     #make the root dir
-    origin = os.path.abspath(origin)
+    #origin = os.path.abspath(origin)
     origin_name = origin.split('/')[-1]
-    replica = os.path.abspath(replica)
+    #replica = os.path.abspath(replica)
     replica = replica + '/' + origin_name 
-    os.mkdir(replica, os.stat(origin).st_mode)
-    copy_folder_content(origin + '/' + origin_name, replica, out)
+    if not os.path.isdir(replica):
+        os.mkdir(replica, os.stat(origin).st_mode)
+        print(str(datetime.datetime.now()) + origin_name + ' made folder')
+    copy_folder_content(origin, replica, out)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         src_dir = sys.argv[1]
     else:
-        src_dir = os.getcwd()
+        src_dir = "/home/cryptid/Programs/Python/Veeam/TESTdir/original/simple"
     if len(sys.argv) > 2:
         dest_dir = sys.argv[2]
-    print(src_dir.split('/')[-1])
-    printls(src_dir)
-
+    else:
+        dest_dir = "/home/cryptid/Programs/Python/Veeam/TESTdir/replica"
+    #print(src_dir)
+    #printls(src_dir)
+    copy_folder(src_dir, dest_dir)
