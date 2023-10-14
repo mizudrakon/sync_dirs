7f8b1b0a66e1b8455454eb73d509024dae416965
mizudrakon <mizudrakon@gmail.com>
2023-10-14 10:32:48 +0200

    mostly done

--- sync_py/src/arguments.py
+++ sync_py/src/arguments.py
@@ -8,28 +8,37 @@ def parse_arguments():
     """parses the arguments for the synchronization script, only the source folder address is required, the rest is optional"""
     parser = argparse.ArgumentParser()
     parser.add_argument("source", help="address of the source folder for sync")
-    parser.add_argument("-d","--destination", help="adress to store the replica folder", default=os.getcwd())
-    parser.add_argument("-o","--output", help="specify the desired log file location", default=os.getcwd())
-    parser.add_argument("-t","--time", help="specify the time interval",type=int, default=60)
+    parser.add_argument("destination", help="adress to store the replica folder", default=os.getcwd())
+    parser.add_argument("-o","--output", help="specify the desired log file location", default=os.getcwd() + "/sync.log")
+    parser.add_argument("-t","--time", help="specify the time interval",type=int, default=30)
+    parser.add_argument("-m","--minute", help="specify the time interval unit to minutes",action="store_true")
+    parser.add_argument("--hour", help="specify the time interval unit to hours",action="store_true")
     return parser.parse_args()
 
-def linux_home(args):
+#remove the end slash, change ~ into home dir on linux
+def format_fix(args):
     for arg in [args.source, args.destination, args.output]:
         if sys.platform == 'linux' and arg[0] == '~':
             arg = os.getenv('HOME')+arg[1:]
+        if arg[-1] == '\\' or arg[-1] == '/':
+            arg = arg[:-1]
     return args
 
 def file_system_check(args):
     """checks if the source folder and destination exists"""
     if not os.path.isdir(args.source) or not os.path.isdir(args.destination): 
+        print("ERROR: source or destination are not real addresses!")
         return False
-    if not os.path.isdir(args.output):
+    if not os.path.isdir(args.output) and not os.path.isdir(os.path.dirname(args.output)):
         return False
+    if os.path.isdir(args.output):
+        args.output = args.output + '/sync.log'
     return True
 
+#testing
 if __name__ == '__main__':
     args = parse_arguments()
-    args = linux_home(args)
+    args = format_fix(args)
     print(args)
     if not file_system_check(args):
         print("Error in user input!")
--- sync_py/src/sync.py
+++ sync_py/src/sync.py
@@ -7,34 +7,20 @@ import datetime
 #I'm just using '/' as delimiter in paths becaue I expect os and shutil to handle it in Windows too
 #I'll test it later and switch to a platform dependant variable if it doesn't work
 
-# t is the required number of \t when printing, f is just check if we're supposed to print files
-def printls(path = '.',tab='\t', t = 0,f = True):
-    """Function to simply write out a folder contents recursively"""
-    # make a list using os.scandir()
-    L = [e for e in os.scandir(path)]
-    for e in L:
-        #skip symlinks or hidden files
-        if e.is_symlink() or e.name[0] == '.':
-            continue
-        #if file and we want file: print
-        if f and e.is_file():
-            print(tab*t,end='')
-            print(e.name + ' modified:' + str(e.stat().st_mtime))
-            continue
-        #if dir go deeper
-        if  e.is_dir():
-            print(tab*t,end='')
-            print(e.name + '/')
-            printls(path + '/' + e.name,tab,t+1,f)
+def print_msg(msg, file, quiet = False):
+    """Function to write messages both to stdout and file"""
+    if not quiet:
+        print(msg)
+    file.write(msg + '\n')
 
-def remove_dir(dir_path):
+def remove_dir(dir_path,log_file):
     """Function that removes all contents of a specified directory and the directory itself"""
     #remove the directory contents
     Ls = [entry for entry in os.scandir(dir_path)]
     for entry in Ls:
         if entry.is_file():
             os.remove(entry)
-            print(str(datetime.datetime.now()) + ' ' + dir_path + '/' + entry.name + ' file removed')
+            print_msg(str(datetime.datetime.now()) + ' ' + dir_path + '/' + entry.name + ' file removed', log_file)
             continue
         #if dir, remove each file first
         if  entry.is_dir():
@@ -44,7 +30,7 @@ def remove_dir(dir_path):
     print(str(datetime.datetime.now()) + ' ' + dir_path + ' folder removed')
     return True
 
-def sync_folder_content(origin, replica, out):
+def sync_folder_content(origin, replica, log_file):
     """Copy contents of the origin folder to the replica folder"""
     #getting contents of the original directory
     Ls = [entry for entry in os.scandir(origin)]
@@ -66,48 +52,49 @@ def sync_folder_content(origin, replica, out):
             #copy file if it doesn't exist, or copy over if it needs updating
             if not os.path.isfile(replica_entry_path) or entry.stat().st_mtime > os.stat(replica_entry_path).st_mtime:
                 shutil.copy2(entry,replica) 
-                print(str(datetime.datetime.now()) + ' ' + entry.path + ' copied file')
+                print_msg(str(datetime.datetime.now()) + ' ' + entry.path + ' - file copied', log_file)
                 continue
         if entry.is_dir():
             #make dir with same rights and content if it doesn't exist
             #we don't copytree() to have more control and report each file copied
             if not os.path.isdir(replica_entry_path):
                 os.mkdir(replica_entry_path,entry.stat().st_mode)
-                print(str(datetime.datetime.now()) + ' ' + replica_entry_path + ' made folder')
+                print_msg(str(datetime.datetime.now()) + ' ' + replica_entry_path + ' - folder made', log_file)
             #even if it does, we need to enter it and check its content
-            sync_folder_content(origin + '/' + entry.name, replica_entry_path, out)
+            sync_folder_content(origin + '/' + entry.name, replica_entry_path, log_file)
     #removing deleted files and directories 
     for entry in replica_Ls:
         path_to_entry = replica + '/' + entry
         if not At[replica_Ls.index(entry)]:
             if os.path.isfile(path_to_entry):
                 os.remove(path_to_entry)
-                print(str(datetime.datetime.now()) + ' ' + path_to_entry + ' file removed')
+                print_msg(str(datetime.datetime.now()) + ' ' + path_to_entry + ' file removed', log_file)
                 continue
             if os.path.isdir(path_to_entry):
-                remove_dir(path_to_entry)
+                remove_dir(path_to_entry, log_file)
 
 
 #first preparation function for the copy_folder_content
-def sync_folder(origin, replica, out = '.'):
+def sync_folder(origin, replica, log_file_name):
     """Creates or enters the replica folder and starts the process"""
-    #make the root dir
+    #open a log file in append
+    log_file = open(log_file_name,'a')
+
+    print_msg(str(datetime.datetime.now()) + ' started synch process on: ' + origin, log_file)
     origin_name = os.path.basename(origin)
     replica = replica + '/' + origin_name 
     if not os.path.isdir(replica):
+    #make the root dir
         os.mkdir(replica, os.stat(origin).st_mode)
-        print(str(datetime.datetime.now()) + origin_name + ' made folder')
-    sync_folder_content(origin, replica, out)
+        print_msg(str(datetime.datetime.now()) + ' ' + origin_name + ' made folder', log_file)
+    sync_folder_content(origin, replica, log_file)
+    log_file.close()
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         src_dir = sys.argv[1]
-    else:
-        #sys.exit(1)
-        src_dir = "/home/cryptid/Programs/Python/Veeam/TESTdir/original/simple"
     if len(sys.argv) > 2:
         dest_dir = sys.argv[2]
     else:
-        #sys.exit(1)
-        dest_dir = "/home/cryptid/Programs/Python/Veeam/TESTdir/replica"
+        sys.exit(1)
     sync_folder(src_dir, dest_dir)
