c85b443f46c4ea691cd94e1214b925f4a827feb6
mizudrakon <mizudrakon@gmail.com>
2023-10-14 12:28:19 +0200

    changed main into run

--- sync_py/src/folder_sync_py/__init__.py
+++ sync_py/src/folder_sync_py/__init__.py
@@ -1,1 +1,2 @@
 """A program to create a replica folder and keep it up with the original"""
+from main import *
--- sync_py/src/folder_sync_py/run.py
+++ sync_py/src/folder_sync_py/run.py
@@ -9,7 +9,16 @@ def signal_handler(num,frame):
     print("Synchronization process stopped")
     sys.exit(0)
 
+def get_args():
+    """Manages arguments from command line"""
+    args = parse_arguments()
+    args = arg_format_fix(args)
+    if not arg_file_system_check(args):
+        sys.exit(1)   
+    return args 
+
 def synchronize(src, dest, log, sync_time, min = False, hour = False):
+    """Encapsulates the whole process of synchronization"""
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTSTP, signal_handler)
     
@@ -35,33 +44,9 @@ def synchronize(src, dest, log, sync_time, min = False, hour = False):
 
 
 if __name__ == '__main__':
-    
-    #signal.signal(signal.SIGINT, signal_handler)
-    #signal.signal(signal.SIGTSTP, signal_handler)
-    
     args = parse_arguments()
     args = arg_format_fix(args)
     if not arg_file_system_check(args):
         sys.exit(1)    
-    
+    args = get_args()
     synchronize(args.source, args.destination, args.output, args.time, args.minute, args.hour)
-    
-    #unit = "seconds"
-    #if args.minute:
-    #    schedule.every(args.time).minutes.do(sync_folder,args.source,args.destination, args.output)
-    #    unit = "minutes"
-    #elif args.hour:
-    #    schedule.every(args.time).hours.do(sync_folder,args.source,args.destination, args.output)
-    #    unit = "hours"
-    #else:
-    #    schedule.every(args.time).seconds.do(sync_folder,args.source,args.destination, args.output)
-
-    #write first log message
-#    with open(args.output, 'a') as log_file:
-      #  print_msg("synchronizing " + args.destination + " to " + args.source + " with " + str(args.time) + ' ' + unit + " interval", log_file)
-    #do first sync
- #   sync_folder(args.source, args.destination, args.output)
-    #continue on schedule
-  #  while (True):
-   #     schedule.run_pending()
-    #    time.sleep(1)
